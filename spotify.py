6b44aaf5d23248976fd08835e254e201da7b8051
iuven1s <87498625+iuven1s@users.noreply.github.com>
2022-03-05 20:31:16 +0000

    Getting access token

---
+++ spotify.py
@@ -0,0 +1,28 @@
+import os
+import json
+import requests
+
+CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+AUTH_URL = "https://accounts.spotify.com/api/token"
+BASE_URL = "https://api.spotify.com/v1/"
+
+
+def get_access_token(url, client_id, client_secret):
+    params = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+        }
+    auth_resp = requests.post(url, params)
+    auth_resp_json = auth_resp.json()
+    return auth_resp_json['access_token']
+
+
+def main():
+    access_token = get_access_token(AUTH_URL, CLIENT_ID, CLIENT_SECRET)
+    print(access_token)
+
+
+if __name__ == "__main__":
+    main()
