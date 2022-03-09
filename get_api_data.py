39e881b112511ef04dc7371b23520e948cf08591
iuven1s <87498625+iuven1s@users.noreply.github.com>
2022-03-09 06:31:50 +0000

    Fix for duplicate tracks + comments

--- get_api_data.py
+++ get_api_data.py
@@ -1,5 +1,10 @@
+"""
+Helper functions to make requests to Spotify's APIs.
+"""
+
 import os
 import requests
+from time import sleep
 
 # Set CLIENT_ID and CLIENT_SECRET as environment variables
 CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
@@ -9,14 +14,27 @@ AUTH_URL = "https://accounts.spotify.com/api/token"
 BASE_URL = "https://api.spotify.com/v1/"
 
 
-def get_access_token(url, client_id, client_secret):
+def get_access_token(client_id, client_secret):
+    """Gets OAuth2 Bearer token (for making API calls)
+
+    Args:
+        client_id (str): Client ID from Spotify
+        client_secret (str): Client Secret from Spotify
+
+    Raises:
+        SystemExit: Raised if authentication fails
+
+    Returns:
+        str: Bearer token
+    """
+    # Using client credentials flow.
     params = {
         "grant_type": "client_credentials",
         "client_id": client_id,
         "client_secret": client_secret
     }
     try:
-        response = requests.post(url, params)
+        response = requests.post(AUTH_URL, params)
         response.raise_for_status()
         access_token_header = {
             "Authorization": f"Bearer {response.json()['access_token']}"
@@ -27,6 +45,18 @@ def get_access_token(url, client_id, client_secret):
 
 
 def multiple_audio_features_endpoint(ids, headers):
+    """Get Audio Features on multiple tracks (max 100)
+
+    Args:
+        ids (str): Comma delimited track ids (e.g. abc,def,ghi)
+        headers (str): Header containing bearer token
+
+    Raises:
+        SystemExit: Raised if 200 is not received
+
+    Returns:
+        dict: Audio features for every track in query
+    """
     try:
         response = requests.get(
             BASE_URL + f"audio-features?ids={ids}",
@@ -38,7 +68,21 @@ def multiple_audio_features_endpoint(ids, headers):
         raise SystemExit(error) from error
 
 
-def playlist_tracks_endpoint(id, headers, **query_args):
+def playlist_tracks_endpoint(playlist_id, headers, **query_args):
+    """Gets all tracks from a playlist (max 100). Accepts optional query
+    arguments
+
+    Args:
+        playlist_id (str): Playlist id
+        headers (str): Header containing bearer token
+
+    Raises:
+        SystemExit: Raised if 200 is not received
+
+    Returns:
+        dict: All tracks in a playlist if there are 100 tracks or less,
+        otherwise returns first 100 tracks of a playlist
+    """
     if query_args is not None:
         args = [f"{param}={val}" for param, val in query_args.items()]
         query_str = "?" + "&".join(args)
@@ -47,19 +91,31 @@ def playlist_tracks_endpoint(id, headers, **query_args):
 
     try:
         response = requests.get(
-            BASE_URL + f"playlists/{id}/tracks" + query_str,
+            BASE_URL + f"playlists/{playlist_id}/tracks" + query_str,
             headers=headers
         )
         response.raise_for_status()
         return response.json()
     except requests.exceptions.HTTPError as error:
-        raise SystemExit(error) from error
+            raise SystemExit(error) from error
 
 
-def general_endpoint(uri, headers):
+def general_endpoint(href, headers):
+    """General endpoint function. Assumes href is well-formed and complete
+
+    Args:
+        href (str): Complete href for Spotify's APIs
+        headers (str): Header containing bearer token
+
+    Raises:
+        SystemError: Raised if 200 is not received
+
+    Returns:
+        dict: Data requested from API
+    """
     try:
         response = requests.get(
-            uri,
+            href,
             headers=headers
         )
         response.raise_for_status()
